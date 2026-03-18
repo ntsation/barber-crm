f3f1f15f12d5f2b14dfe0989dcd92bdc7c6a5362
Nathan Souza <naathanmarinho11@gmail.com>
2026-03-18 17:06:28 -0300

    remove unused API router files for barbershop and user management

--- app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.api.v1 import user_router, barbershop_router
-
-api_router = APIRouter()
-api_router.include_router(user_router.router, prefix="/users", tags=["users"])
-api_router.include_router(
-    barbershop_router.router, prefix="/barbershops", tags=["barbershops"]
-)
