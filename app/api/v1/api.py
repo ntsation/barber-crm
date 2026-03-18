ceb5cc1c1f6c6464da5ec915fc8fcd04fc454a6c
Nathan Souza <naathanmarinho11@gmail.com>
2026-03-18 17:01:34 -0300

    add barbershop router to API for managing barbershop resources

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1 import user_router
+from app.api.v1 import user_router, barbershop_router
 
 api_router = APIRouter()
 api_router.include_router(user_router.router, prefix="/users", tags=["users"])
+api_router.include_router(
+    barbershop_router.router, prefix="/barbershops", tags=["barbershops"]
+)
