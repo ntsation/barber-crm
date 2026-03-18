eb82446f5a2d83655d4587fa4ac212cc42c6842d
Nathan Souza <naathanmarinho11@gmail.com>
2026-03-18 17:06:47 -0300

    refactor base class to improve database model structure and clarity

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,11 +1,19 @@
+from sqlalchemy import Column, DateTime, Boolean, func
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
 
 @as_declarative()
 class Base:
     id: any
     __name__: str
-    
-    # Generate __tablename__ automatically
+
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime, server_default=func.now(), onupdate=func.now(), nullable=False
+    )
+    is_active = Column(Boolean, default=True, nullable=False)
+    deleted_at = Column(DateTime, nullable=True)
+
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
